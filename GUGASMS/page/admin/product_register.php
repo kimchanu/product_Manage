34907289d32529f4e434b3fc57f5a4a6bfacd1ca
chanu <cksdn2651@gmail.com>
2023-11-27 16:57:50 +0900

    abc

---
+++ GUGASMS/page/admin/product_register.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="kr">
+<head>
+	<meta charset="UTF-8">
+	<!-- <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	
+	<title>메시지전송</title>
+	<!-- FONT -->
+	<link href="https://fonts.googleapis.com/css?family=Gothic+A1:100,200,300,400,500,700,800,900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/common.css<?php echo $this->version; ?>" />
+	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/reset.css<?php echo $this->version; ?>" />
+	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/admin.css<?php echo $this->version; ?>" />
+	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/adm_sub.css<?php echo $this->version; ?>" />
+	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/admin.modal.css<?php echo $this->version;?>"/>
+	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/adm_table.css<?php echo $this->version; ?>" />
+	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/common2.css<?php echo $this->version; ?>" />
+	<!-- FONTAWESOME -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+	<link rel="stylesheet" href="GUGASMS/page/admin/css/jquery-ui.min.css">
+	<link rel="stylesheet" href="GUGASMS/page/admin/css/wickedpicker.min.css">
+	<link rel="stylesheet" href="GUGASMS/page/admin/css/timepicki.css">
+	<script src="GUGASMS/page/admin/js/jquery/jquery-3.3.1.min.js"></script>
+	<script src="GUGASMS/page/admin/js/jquery-ui.min.js"></script>
+	<script src="GUGASMS/page/admin/js/wickedpicker.min.js"></script>
+	<script src="GUGASMS/page/admin/js/timepicki.js"></script>
+	
+	<!-- sript시작 -->
+	<script src="GUGASMS/page/admin/js/lb.js<?php echo $this->version; ?>"></script>
+	<script src="GUGASMS/page/admin/js/admin.js<?php echo $this->version;?>"></script>
+
+
+	<script>
+		var session_receiver = <?php echo $receiver?>;
+	</script>
+</head>
+<div class="loading"><img class="loading_img" src ="GUGASMS/page/admin/images/Spinner.gif"></div>
+	<div class="wrap">
+        <?php include_once $this->dir."page/admin/include/admin_sidebar.php";?>
+		<div class="adm_container">
+			<?php include_once $this->dir . "page/admin/include/admin_header.php"; ?>
+			<div class="adm_section_container_head pb-0">
+				<h2>메시지전송</h2>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
