6af049c934749bd6a4b49aef98d679a58453f334
chanu <cksdn2651@gmail.com>
2023-11-27 16:04:19 +0900

    aa

--- GUGASMS/page/admin/include/admin_header.php
+++ GUGASMS/page/admin/include/admin_header.php
@@ -13,49 +13,7 @@
 			</svg>
 		</div> -->
 		<!-- hamburger // -->
-		<div class="header_left" id = "login_header_menu" style = "display:none;">
-			<div class="header-menu">
-				<ul class="depth1-con clearfix">
-					<li class="depth1" data-side="">
-						<a class="depth2-a" id ="msg_side" href="#" onclick = "side_bar(this); return false;">메시지보내기</a>
-						<ul class="depth2-con" style ="display:none;">
-							<li class="depth2"><a href="?ctl=Move&param1=msg_main">메시지전송</a></li>
-							<li class="depth2"><a href="?ctl=Move&param1=msg_reserve">예약메시지</a></li>
-							<li class="depth2 admin_none" ><a href="?ctl=Move&param1=msg_result">전송결과</a></li>
-							<li class="depth2"><a href="?ctl=Move&param1=msg_box">메시지폼관리</a></li>
-						</ul>
-					</li>
-					<li class="depth1" data-side="">
-						<a class="depth2-a" id ="kakao_side" href="#" onclick = "side_bar(this); return false;">카카오톡보내기</a>
-						<ul class="depth2-con" style ="display:none;">
-							<li class="depth2"><a href="?ctl=Move&param1=kakao_main">알림톡전송</a></li>
-<!-- 							<li class="depth2"><a href="?ctl=Move&param1=kakao_f_main">친구톡전송</a></li> -->
-							<li class="depth2 admin_none"><a href="?ctl=Move&param1=kakao_result">전송결과</a></li>
-							<li class="depth2 admin_none"><a href="?ctl=Move&param1=kakao_num_set">발신번호관리</a></li>
-							<li class="depth2 admin_none"><a href="?ctl=Move&param1=kakao_pf_set">발신프로필관리</a></li>
-							<li class="depth2 admin_none"><a href="?ctl=Move&param1=kakao_tpl_set">알림톡템플릿관리</a></li>
-						</ul>
-					</li>
-					<li class="depth1"  data-side="">
-						<a class="depth2-a" id ="addr_side" href="#" onclick = "side_bar(this); return false;">주소록</a>
-						<ul class="depth2-con" style ="display:none;">
-							<li class="depth2"><a href="?ctl=Move&param1=add_main">주소록</a></li>
-							<li class="depth2 admin_none"><a href="?ctl=Move&param1=add_set">주소록그룹관리</a></li>
-						</ul>
-					</li>
-					<li class="depth1"  <?php echo $this->side_bar();?> data-side="">
-						<a class="depth2-a" id ="user_side" href="#" onclick = "side_bar(this); return false;">관리자</a>
-						<ul class="depth2-con" style ="display:none;">
-							<li class="depth2"><a href="?ctl=Move&param1=user_set">사용자관리</a></li>
-							<li class="depth2"><a href="?ctl=Move&param1=state_day">일별통계</a></li>
-							<li class="depth2"><a href="?ctl=Move&param1=state_month">월별통계</a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-		</div>
 		<div>
-			<h3 class="adm_logo c-white adm_logo_on"><a href="?">메시지 발송 시스템</a></h3>
 			<div class="adm_utill" id ="logout_elem" style = "display:none;">
 				
 				<span class="header-user-info" id = "login_name"><em></em></span>
