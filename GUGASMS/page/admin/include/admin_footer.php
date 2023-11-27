5b5e154971a86c38e9df570e22b56aa5d7350261
chanu <cksdn2651@gmail.com>
2023-11-27 11:32:46 +0900

    mod

--- GUGASMS/page/admin/include/admin_footer.php
+++ GUGASMS/page/admin/include/admin_footer.php
@@ -1,4 +1,4 @@
 			<footer class="adm_footer adm_lang_en">
-				<div class="adm_footer_copy">Copyright © 2020 krinfra. All rights reserved.</div>
+				<div class="adm_footer_copy">Copyright © 2020 koinfra. All rights reserved.</div>
 			</footer>
 			<!-- adm_footer끝  -->	
