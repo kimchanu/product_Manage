f3b7b165eb1c8515a23fc4c22d6ce6b4cc52b4de
chanu <cksdn2651@gmail.com>
2023-11-27 17:18:12 +0900

    aa

--- GUGASMS/page/admin/product_register.php
+++ GUGASMS/page/admin/product_register.php
@@ -9,20 +9,19 @@
 	<title>abcdfad</title>
 	<!-- FONT -->
 	<link href="https://fonts.googleapis.com/css?family=Gothic+A1:100,200,300,400,500,700,800,900&display=swap" rel="stylesheet">
+	
 	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/reset.css<?php echo $this->version;?>"/>
 	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/common.css<?php echo $this->version;?>"/>
 	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/admin.css<?php echo $this->version;?>"/>
 	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/admin_sub.css<?php echo $this->version;?>"/>
-	<!-- FONTAWESOME -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
-	<link rel="stylesheet" href="GUGASMS/page/admin/css/jquery-ui.min.css">
-	<link rel="stylesheet" href="GUGASMS/page/admin/css/wickedpicker.min.css">
-	<link rel="stylesheet" href="GUGASMS/page/admin/css/timepicki.css">
-	
+	<!-- 2019-11-21 추가 -->
+	<link rel="stylesheet" type="text/css" href="GUGASMS/page/admin/css/common2.css<?php echo $this->version;?>"/>
+
 	<!-- sript시작 -->
-    <script src="GUGASMS/page/admin/js/jquery/jquery-3.3.1.min.js"></script>
-	<script src="GUGASMS/page/admin/js/lb.js<?php echo $this->version; ?>"></script>
-	<script src="GUGASMS/page/admin/js/admin.js<?php echo $this->version;?>"></script>
+	<script src="GUGASMS/page/admin/js/jquery/jquery-3.3.1.min.js"></script>
+	<script src="GUGASMS/page/admin/js/lb.js<?php echo $this->version;?>"></script>
+    <script src="GUGASMS/page/admin/js/admin.js<?php echo $this->version;?>"></script>
+	<!-- script끝 -->
 </head>
 
     <body>
