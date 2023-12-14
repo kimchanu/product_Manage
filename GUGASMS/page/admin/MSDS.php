c007e7d9b632ab4184846a64c283a2f96014d15d
chanu <cksdn2651@gmail.com>
2023-12-14 13:34:56 +0900

    ab

--- GUGASMS/page/admin/MSDS.php
+++ GUGASMS/page/admin/MSDS.php
@@ -36,7 +36,7 @@
 			</div>
 			<div class="adm_main_container">
 				<div class="clearfix row">
-					<div class="col-md-7 col-lg-12 adm_section_container_mid">
+					<div class="col-md-7 col-lg-12">
 						<section class="adm_section_container container_mid">
 						<form action="">
 							<table id="sch_box" class = "utill">
