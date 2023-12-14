7e7f3f80f00a615339577b898ed34ef0bacb253e
chanu <cksdn2651@gmail.com>
2023-12-14 14:13:36 +0900

    aa

--- GUGASMS/page/admin/MSDS.php
+++ GUGASMS/page/admin/MSDS.php
@@ -60,7 +60,7 @@
 								</tr>
 								</tbody></table>
 						</form>
-						<div id="content" class = "display_msds">
+						<div class = "display_msds">
 							<h1 class="">화학물질정보</h1>
 							<table class="">
 								<tbody><tr>
@@ -75,7 +75,7 @@
 								</tr>
 											</tbody></table>
 						
-							<ul class="">
+							<ul class="" style="float : left">
 								<li><a onclick="history.back()">목록</a></li>
 							</ul>
 							<ul class="">
