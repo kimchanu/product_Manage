b8b8b14c44f94e2661b81b909c4d4e7cbb460468
chanu <cksdn2651@gmail.com>
2023-12-18 09:59:25 +0900

    abc

--- GUGASMS/page/admin/js/kakao_f_main.js
+++ GUGASMS/page/admin/js/kakao_f_main.js
@@ -5,8 +5,13 @@ $(document).ready(function(){
         url: "https://msds.kosha.or.kr/openapi/service/msdschem/chemlist?serviceKey=3h5o8Pm7wctZGh8LPV70Bcu0Z38%2BE3swtfbcO3%2F4%2FLRh3E9E62nhm5o1JPLHNs7Sxb3MOxucrHIFmzxYWoOt8A%3D%3D&searchWrd=wd&searchCnd=0",
         dataType : 'jsonp',
         success : function(data){
+          console.log('hihihi')
           console.log(location.origin)
            console.log(data)
+           
+        }
+        , error : function(xhr, status, error){
+          alert(xhr.status, xhr.responseText, error);
         }
 
       })
