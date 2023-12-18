f57a8a20392a6e5ee0e7a3170f07055187357978
chanu <cksdn2651@gmail.com>
2023-12-18 10:11:30 +0900

    abc

--- GUGASMS/page/admin/js/kakao_f_main.js
+++ GUGASMS/page/admin/js/kakao_f_main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
     $.ajax({
+        headers: {  'Access-Control-Allow-Origin': "https://msds.kosha.or.kr/openapi/service/msdschem/chemlist?serviceKey=3h5o8Pm7wctZGh8LPV70Bcu0Z38%2BE3swtfbcO3%2F4%2FLRh3E9E62nhm5o1JPLHNs7Sxb3MOxucrHIFmzxYWoOt8A%3D%3D&searchWrd=wd&searchCnd=0"},
         type: "GET",
         url: "https://msds.kosha.or.kr/openapi/service/msdschem/chemlist?serviceKey=3h5o8Pm7wctZGh8LPV70Bcu0Z38%2BE3swtfbcO3%2F4%2FLRh3E9E62nhm5o1JPLHNs7Sxb3MOxucrHIFmzxYWoOt8A%3D%3D&searchWrd=wd&searchCnd=0",
         dataType : 'xml',
