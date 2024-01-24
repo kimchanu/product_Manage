6e20de854cfa2990151ed1ffa76f3d4c10431473
chanu <cksdn2651@gmail.com>
2024-01-24 09:04:31 +0900

    aa

--- GUGASMS/page/admin/input.php
+++ GUGASMS/page/admin/input.php
@@ -152,31 +152,31 @@
                                         <div class="wi_board_form_title" style="background-color: yellow">
                                             <label for="" class="wi_board_label_control" style="background-color: green">자재코드</label>
                                         </div>
-                                        <div class="wi_board_form_content" style="background-color: red">
+                                        <div class="wi_board_form_content2" style="background-color: red">
                                             <input
                                                 type="text"
                                                 id="product_code"
-                                                class="wi_board_input_control2"
+                                                class="wi_board_input_control"
                                                 autocomplete="off">
                                         </div>
                                         <div class="wi_board_form_title" style="background-color: yellow">
                                             <label for="" class="wi_board_label_control" style="background-color: green">자재코드</label>
                                         </div>
-                                        <div class="wi_board_form_content" style="background-color: red">
+                                        <div class="wi_board_form_content2" style="background-color: red">
                                             <input
                                                 type="text"
                                                 id="product_code"
-                                                class="wi_board_input_control2"
+                                                class="wi_board_input_control"
                                                 autocomplete="off">
                                         </div>
                                         <div class="wi_board_form_title" style="background-color: yellow">
                                             <label for="" class="wi_board_label_control" style="background-color: green">자재코드</label>
                                         </div>
-                                        <div class="wi_board_form_content" style="background-color: red">
+                                        <div class="wi_board_form_content2" style="background-color: red">
                                             <input
                                                 type="text"
                                                 id="product_code"
-                                                class="wi_board_input_control2"
+                                                class="wi_board_input_control"
                                                 autocomplete="off">
                                         </div>
                                     </div>
