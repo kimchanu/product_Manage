859294b950df9d0b77f233591da26c2c8a8956c1
chanu <cksdn2651@gmail.com>
2024-03-21 09:28:05 +0900

    aa

--- GUGASMS/page/admin/js/msg_box.js
+++ GUGASMS/page/admin/js/msg_box.js
@@ -150,7 +150,7 @@ function init_search(){
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    content = document.querySelector( "contenteditable" );
+    content = document.querySelector( "[contenteditable]" );
     console.log(content);
 
     // @breif 더블클릭시 실행
